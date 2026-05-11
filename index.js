01a7ccec2f45fb16f0530d59f3fc60d07f0a19b2
DinisNunes24 <deniden.lupical@gmail.com>
2026-05-11 09:56:29 +0100

    Update index.js
    fetch dentro de uma função e criação da função tratar_resp

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const options = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDM0ZjVkOGE1NDRhNDQxNmU2NTk2ZTU4ZDcxMjM4ZSIsIm5iZiI6MTc3Nzk3MjI1MS40ODYsInN1YiI6IjY5ZjliNDFiOGIwOWY5MTc5ZmM5NWIxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aarhlhmz4GTF1nxm6PpmxMhg3Tr1pzBrNIKTE0TCVlo'
   }
 };
+response();
 
+function response(){
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=8d34f5d8a544a4416e6596e58d71238e', options)
   .then(res => res.json())
   .then(res => console.log(res))
+  .then(function (res){
+    tratar_resp(res);
+  })
   .catch(err => console.error(err));
+}
+
+function tratar_resp(res){
+ console.log("hey")
+
+}
