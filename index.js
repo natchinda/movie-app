009ef58b1e64eb5ef13c241f450077bfef2956be
Milca <natchindamilca@gmail.com>
2026-05-12 14:39:04 +0100

    Correct async function and movie display code
    Fix syntax errors and improve movie display logic.

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const container = document.getElementById("moviesContainer");
 async function getPopularMovies();
   const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}');
 
-const data = awai response.json();
+const data = await response.json();
   displayMovies(data.results);
 }
 
 function displayMovies(movies) {
   container.innerHTML = "";
 
-  movies.forEach(movies => {
-    const movieDiv = document.create.Element("div");
-    moviediv.classList.add("movie");
+  movies.forEach(movie => {
+    const movieDiv = document.createElement("div");
+    movieDiv.classList.add("movie");
 
     movieDiv.innerHTML ='
-      <h3>${movie.tatle}</h3>
+      <h3>${movie.tttle}</h3>
       <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" />
       <p>${movie.vote_average}</p>
   ;
