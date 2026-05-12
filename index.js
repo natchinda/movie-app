0ebe90cfb127013e9f7f3d18907f42ca2c164b58
Milca <natchindamilca@gmail.com>
2026-05-12 14:18:41 +0100

    Implement function to fetch and display popular movies
    Refactor movie fetching and displaying logic.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const API_KEY = '8d34f5d8a544a4416e6596e58d71238e'
-const BASE_URL = 'https://api.themoviedb.org/3'
+const container = document.getElementById("moviesContainer");
 
-window.onload = () => buscarFilmes(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt`);
+async function getPopularMovies();
+  const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}');
+
+const data = awai response.json();
+  displayMovies(data.results);
+}
+
+function displayMovies(movies) {
+  container.innerHTML = "";
+
+  movies.forEach(movies => {
+    const movieDiv = document.create.Element("div");
+    moviediv.classList.add("movie");
+
+    movieDiv.innerHTML ='
+      <h3>${movie.tatle}</h3>
+      <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" />
+      <p>${movie.vote_average}</p>
+  ;
+   container.appendChild(movieDiv);
+});
+}
+getPopularMovies();
 
 const options = {
   method: 'GET',
